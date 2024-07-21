a74039a85c7eeadc0a7eb93db67e6acbe6fd93ce
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-07-21 13:44:51 -0400

    Add basic error message

---
+++ how-say.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export PYTHONPATH="/Users/benlandrette/ccode/third-party-libs/eng_to_ipa/env/lib/python3.12/site-packages:$PYTHONPATH"
+python3 /Users/benlandrette/ccode/serious-projects/word-pronunciation/script.py "$@"
--- script.py
+++ script.py
@@ -1,23 +1,5 @@
-import eng_to_ipa as ipa
 import sys
-
-words = {}
-with open("words.txt") as file:
-    for line in file:
-        word = line.strip()
-        ipa_words = ipa.ipa_list(word)[0]
-        words[word] = ipa_words
-
-
-def display_words(words):
-    for key in words:
-        print(f"{key}:")
-        for word in words[key]:
-            print(f" - {word}")
-        print("")
-
-
-# display_words(words)
+import eng_to_ipa as ipa
 
 
 def main(args):
@@ -25,6 +7,8 @@ def main(args):
         word = args[1]
         result = ipa.convert(word)
         print(f"{word}: {result}")
+    else:
+        print("Please provide a word.")
 
 
 if __name__ == "__main__":
